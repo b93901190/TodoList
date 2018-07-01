7b501607eb3f37c7c0045aa39d38bd89aaea3ab9
TSUNG-CHUAN MA <cutekiawe@gmail.com>
2018-06-30 23:54:16 -0700

    add css style
    add stopPropagation()
    add parent and learn heirachical $(this)
    add css style

--- assets/js/todoList.js
+++ assets/js/todoList.js
@@ -1,8 +1,22 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 // click on X to delete todo
-$("span").click(function(){
-
+$("ul").on("click", "span", function(e){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
 });
+
+$("input[type='text']").keypress(function(e){
+    if(e.which === 13){
+        // new todo list from input
+        var todoText = $(this).val();
+        $(this).val("");
+        // new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
+
